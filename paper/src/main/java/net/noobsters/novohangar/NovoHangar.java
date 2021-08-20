3cb9a747331730f5444f8571d0dc84b28f477b99
Juan Cedeno <henixceo@gmail.com>
2021-08-20 14:01:59 -0400

    intercepting packets

--- paper/src/main/java/net/noobsters/novohangar/NovoHangar.java
+++ paper/src/main/java/net/noobsters/novohangar/NovoHangar.java
@@ -1,5 +1,15 @@
 package net.noobsters.novohangar;
 
+import java.lang.reflect.Field;
+
+import com.comphenix.protocol.PacketType;
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+import com.comphenix.protocol.events.ListenerPriority;
+import com.comphenix.protocol.events.PacketAdapter;
+import com.comphenix.protocol.events.PacketEvent;
+import com.comphenix.protocol.wrappers.EnumWrappers.PlayerInfoAction;
+
 import org.bukkit.Bukkit;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
@@ -8,19 +18,73 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import io.papermc.paper.event.player.AsyncChatEvent;
 import kr.entree.spigradle.annotations.SpigotPlugin;
+import lombok.Getter;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import us.jcedeno.libs.rapidinv.RapidInvManager;
 
-@SpigotPlugin
+@SpigotPlugin // Use @SpigotPlugin from spigradle to generate plugin.yml
 public class NovoHangar extends JavaPlugin implements Listener {
-    private static MiniMessage miniMessage = MiniMessage.get();
+    /** Mini message to easily parse messages. */
+    private @Getter static MiniMessage miniMessage = MiniMessage.get();
+    /** Protocol Lib to intercept packages with ease. */
+    private @Getter ProtocolManager protocolManager;
+
+    @Override
+    public void onLoad() {
+        protocolManager = ProtocolLibrary.getProtocolManager();
+    }
 
     @Override
     public void onEnable() {
         // Register RapidInvManager
         RapidInvManager.register(this);
         Bukkit.getPluginManager().registerEvents(this, this);
+        protocolManager.addPacketListener(
+                new PacketAdapter(this, ListenerPriority.NORMAL, PacketType.Play.Server.SPAWN_ENTITY) {
+
+                });
+        protocolManager.addPacketListener(
+                new PacketAdapter(this, ListenerPriority.NORMAL, PacketType.Play.Server.PLAYER_INFO) {
+                    @Override
+                    public void onPacketSending(PacketEvent event) {
+                        if (event.getPacketType() == PacketType.Play.Server.PLAYER_INFO) {
+                            var packet = event.getPacket();
+                            // Reference https://wiki.vg/Protocol#Player_Info
+                            var action = packet.getPlayerInfoAction().read(0);
+                            if (action.compareTo(PlayerInfoAction.ADD_PLAYER) == 0/** 0 = Add Player */
+                            ) {
+                                var info = packet.getPlayerInfoDataLists().read(0);
+                                int count = 0;
+                                for (var entry : info) {
+                                    var c_profile = entry.getProfile().withName("HolaPinche");
+
+                                    var fields = c_profile.getHandle().getClass().getDeclaredFields();
+                                    for (var field : fields) {
+                                        System.out.println("Fields: " +field.getName());
+                                    }
+                                    count++;
+
+                                }
+                                packet.getPlayerInfoDataLists().write(0, info);
+
+                            }
+                        }
+                    }
+                });
+    }
+
+    public static void setValue(Object object, String field, Object value) {
+        try {
+            Field f = object.getClass().getDeclaredField(field);
+            if (!f.isAccessible()) {
+                f.setAccessible(true);
+            }
+            f.set(object, value);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @EventHandler
@@ -35,6 +99,12 @@ public class NovoHangar extends JavaPlugin implements Listener {
             player.sendMessage(msg);
         }
 
+    }
+
+    @EventHandler
+    public void onChatEvent(AsyncChatEvent e) {
+        var player = e.getPlayer();
+        player.sendMessage(miniMessage.parse("Why are you not talking!"));
     }
 
 }
--- replace-debug.sh
+++ replace-debug.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 chcp.com 65001
 # Build
-./gradlew build
+./gradlew :paper:build
 # Copy the plugin into folder
 cp paper/build/libs/*-all.jar paper/debug/spigot/plugins/
