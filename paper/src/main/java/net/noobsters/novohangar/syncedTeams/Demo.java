6887c988e1b47dfe0153c13cd60c320c1ffce676
Juan Cedeno <henixceo@gmail.com>
2021-09-13 18:11:16 -0400

    Sync pippeline ready

--- paper/src/main/java/net/noobsters/novohangar/syncedTeams/Demo.java
+++ paper/src/main/java/net/noobsters/novohangar/syncedTeams/Demo.java
@@ -1,13 +1,83 @@
 package net.noobsters.novohangar.syncedTeams;
 
+import com.google.gson.Gson;
+
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.pubsub.RedisPubSubListener;
 import net.noobsters.novohangar.syncedTeams.objects.TeamObject;
 import net.noobsters.novohangar.syncedTeams.objects.User;
 
 public class Demo {
+    private static Gson gson = new Gson();
 
-    public static void main(String[] args) {
-        var user1 = User.generateUser();
-        var user2 = User.generateUser();
+    public static void main(String[] args) throws InterruptedException {
+        /**
+         * Initialize some variables to be used in the demo.
+         */
+        var users = User.generateUsers();
+        var team = TeamObject.createTeamObject("team", users);
+        /** Initiliaze a redis connection. */
+        var redisClient = RedisClient
+                .create(RedisURI.builder().withHost("redis-11764.c73.us-east-1-2.ec2.cloud.redislabs.com")
+                        .withPort(11764).withPassword("Gxb1D0sbt3VoyvICOQKC8IwakpVdWegW").build());
+        var listenerThread = createNewApplicationThread(redisClient);
+
+        listenerThread.start();
+        Thread.sleep(500);
+        redisClient.connectPubSub().async().publish("dedsafio", gson.toJson(team));
+
+        while (true) {
+
+        }
+    }
+
+    private static Thread createNewApplicationThread(RedisClient redisClient) {
+        return new Thread(() -> {
+            var pubSub = redisClient.connectPubSub();
+            var sync = pubSub.sync();
+            pubSub.addListener(new RedisPubSubListener<String, String>() {
+
+                @Override
+                public void message(String channel, String message) {
+                    System.out.println("Message received: " + message + " on channel: " + channel);
+
+                }
+
+                @Override
+                public void message(String pattern, String channel, String message) {
+                    // TODO Auto-generated method stub
+
+                }
+
+                @Override
+                public void subscribed(String channel, long count) {
+                    // TODO Auto-generated method stub
+
+                }
+
+                @Override
+                public void psubscribed(String pattern, long count) {
+                    // TODO Auto-generated method stub
+
+                }
+
+                @Override
+                public void unsubscribed(String channel, long count) {
+                    // TODO Auto-generated method stub
+
+                }
+
+                @Override
+                public void punsubscribed(String pattern, long count) {
+                    // TODO Auto-generated method stub
+
+                }
+
+            });
+            sync.subscribe("dedsafio");
+
+        });
     }
 
 }
--- paper/src/main/java/net/noobsters/novohangar/syncedTeams/objects/User.java
+++ paper/src/main/java/net/noobsters/novohangar/syncedTeams/objects/User.java
@@ -1,5 +1,6 @@
 package net.noobsters.novohangar.syncedTeams.objects;
 
+import java.util.Random;
 import java.util.UUID;
 
 import com.github.javafaker.Faker;
@@ -30,8 +31,32 @@ public class User {
     public static User generateUser() {
         return new User(UUID.randomUUID(), faker.name().firstName());
     }
+
     /**
-     * Function that generates a user with a random name from internet.
+     * Static function that generates a random collection of users
+     * 
+     * @param size The size of the collection.
      */
+    public static User[] generateUsers(int size) {
+        User[] users = new User[size];
+        for (int i = 0; i < size; i++) {
+            users[i] = generateUser();
+        }
+        return users;
+    }
+
+    /**
+     * Static function that generates a random collection of users with a max size
+     * of 10.
+     */
+    public static User[] generateUsers() {
+
+        int size = new Random().nextInt(10) + 1;
+        User[] users = new User[size];
+        for (int i = 0; i < size; i++) {
+            users[i] = generateUser();
+        }
+        return users;
+    }
 
 }
